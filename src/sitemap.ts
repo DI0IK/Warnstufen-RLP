acde2d667246aaccfe9ce7596436a47a1b5c9fb9
DI0IK <76131420+DI0IK@users.noreply.github.com>
2021-10-14 19:24:23 +0200

    sitemap update

---
+++ src/sitemap.ts
@@ -0,0 +1,36 @@
+import * as formats from './formats';
+import { districts } from './districts';
+
+export function genSiteMap(
+	apiRoutes: formats.APIEndpoint[],
+	normalRoutes: formats.Route[]
+): string {
+	const routes = [...apiRoutes, ...normalRoutes];
+	const sitemap = routes
+		.map((route) => {
+			if (!route.path.includes(':lk')) return [route];
+
+			const routes = districts.map((district) => {
+				const newPath = route.path.replace(':lk', district);
+				return { ...route, path: newPath };
+			});
+
+			return routes;
+		})
+		.map((route) => {
+			return route.map((r) => {
+				if (!r.inSitemap) return '';
+				const url = r.path;
+				return `<url>
+					<loc>${url}</loc>
+				</url>`;
+			});
+		})
+		.map((route) => route.filter((r) => r !== ''))
+		.filter((route) => route.length > 0);
+
+	return `<?xml version="1.0" encoding="UTF-8"?>
+   	<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    	${sitemap.map((r) => r.join('\n')).join('\n')}
+	</urlset>`;
+}
