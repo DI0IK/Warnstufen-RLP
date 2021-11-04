7aa603f84683da970a4fd677b871c8180e0e1d56
DI0IK <76131420+DI0IK@users.noreply.github.com>
2021-11-04 15:40:14 +0100

    cors fix?

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const https = require('https');
 const http = require('http');
 const { parse } = require('url');
 const next = require('next');
-const cors = require('cors');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -23,17 +22,15 @@ if (!onVercel) {
 				(req, res) => {
 					const parsedUrl = parse(req.url, true);
 
-					cors({
-						methods: ['GET', 'HEAD'],
-						origin: '*',
-					})(req, res, (err) => {
-						if (err) {
-							res.statusCode = 500;
-							res.end('Internal Server Error');
-						} else {
-							handle(req, res, parsedUrl);
-						}
-					});
+					res.setHeader('Access-Control-Allow-Origin', '*');
+					res.setHeader(
+						'Access-Control-Allow-Methods',
+						'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+					);
+					res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+					res.setHeader('Access-Control-Allow-Credentials', true);
+
+					handle(req, res, parsedUrl);
 				}
 			)
 			.listen(dev ? 3000 : 443, (err) => {
