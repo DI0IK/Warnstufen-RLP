56553e1fc14f4e8c24ae3499910c048ea0f5f71d
DI0IK <76131420+DI0IK@users.noreply.github.com>
2021-11-26 21:41:24 +0100

    fixes

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,24 @@
 export default function Layout({ children }) {
-	return <div className="layout">{children}</div>;
+	if (process.env.WARTUNG === 'true') {
+		return (
+			<div className="layout">
+				<header></header>
+				<main>
+					<h1>Wartungsarbeiten</h1>
+					<p>
+						Wir arbeiten gerade an der Seite. Dies kann einige Minuten bis zu wenigen Tagen
+						dauern.
+					</p>
+				</main>
+				<footer></footer>
+			</div>
+		);
+	}
+	return (
+		<div className="layout">
+			<header></header>
+			<main>{children}</main>
+			<footer></footer>
+		</div>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,8 @@ import Layout from '../components/layout';
 export default function Index() {
 	return (
 		<Layout>
-			<header></header>
-			<main>
-				<h1>Warnzahlen RLP</h1>
-				<p>Diese Seite listet alle vom LUA veröffentlichten Warnzahlen für RLP auf.</p>
-			</main>
-			<footer></footer>
+			<h1>Warnzahlen RLP</h1>
+			<p>Diese Seite listet alle vom LUA veröffentlichten Warnzahlen für RLP auf.</p>
 		</Layout>
 	);
 }
