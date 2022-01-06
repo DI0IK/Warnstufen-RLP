e95d0e43105e14f02aa55524e24bd837a1cdbe09
DI0IK <76131420+DI0IK@users.noreply.github.com>
2022-01-06 17:27:17 +0100

    logs

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (!onVercel) {
 						fs.readFileSync('./certs/server.crt') +
 						fs.readFileSync('./certs/intermediate.crt'),
 				},
-				(req, res) => {
+				async (req, res) => {
 					const parsedUrl = parse(req.url, true);
 
 					res.setHeader('Access-Control-Allow-Origin', '*');
@@ -28,10 +28,41 @@ if (!onVercel) {
 						'Access-Control-Allow-Methods',
 						'GET, POST, OPTIONS, PUT, PATCH, DELETE'
 					);
-					res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+					res.setHeader(
+						'Access-Control-Allow-Headers',
+						'X-Requested-With,content-type,content-length'
+					);
 					res.setHeader('Access-Control-Allow-Credentials', true);
 
-					handle(req, res, parsedUrl);
+					const startTime = Date.now();
+
+					if (parsedUrl.pathname.startsWith('/admin/data.tsv')) {
+						if (
+							req.headers['x-api-key'] !== process.env.API_KEY &&
+							parsedUrl.query?.apiKey !== process.env.API_KEY
+						) {
+							res.statusCode = 403;
+							res.end('Invalid/Missing API key');
+						} else {
+							const { date } = parsedUrl.query;
+							const data = fs.readFileSync(
+								`./logs/${date || new Date().toISOString().split('T')[0]}.tsv`
+							);
+							res.writeHead(200, {
+								'Content-Type': 'text/tab-separated-values',
+								'Content-Length': data.length,
+							});
+							res.end(data);
+							return;
+						}
+					}
+
+					handle(req, res, parsedUrl).then(() => {
+						// skip _next/static
+						if (req.url.startsWith('/_next/static/')) return;
+
+						log(req, res, startTime);
+					});
 				}
 			)
 			.listen(443, (err) => {
@@ -55,14 +86,53 @@ if (!onVercel) {
 		const port = parseInt(process.env.PORT, 10);
 
 		http
-			.createServer((req, res) => {
+			.createServer(async (req, res) => {
 				const parsedUrl = parse(req.url, true);
 
-				handle(req, res, parsedUrl);
+				const startTime = Date.now();
+
+				handle(req, res, parsedUrl).then(() => {
+					// skip _next/static
+					if (req.url.startsWith('/_next/static/')) return;
+
+					log(req, res, startTime);
+				});
 			})
 			.listen(port, (err) => {
 				if (err) throw err;
 				console.log(`> Ready on https://${process.env.VERCEL_HOSTNAME}`);
 			});
 	});
+}
+
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ * @param {number} startTime
+ */
+function log(req, res, startTime) {
+	const logfile = `./logs/${new Date().toISOString().split('T')[0]}.tsv`;
+
+	if (!fs.existsSync('./logs')) {
+		fs.mkdirSync('./logs');
+	}
+	if (!fs.existsSync(logfile)) {
+		fs.writeFileSync(
+			logfile,
+			`Time\tIP\tMethod\tPath\tUser-Agent\tReferer\tStatus\tResponse-Time\tResponse-Length\n`
+		);
+		fs.chmodSync(logfile, 0o666);
+	}
+
+	fs.appendFile(
+		logfile,
+		`${new Date().toISOString()}\t${req.socket.remoteAddress || ''}\t${req.method}\t${req.url}\t${
+			req.headers['user-agent']
+		}\t${req.headers.referer || ''}\t${res.statusCode}\t${Date.now() - startTime}\t${
+			res.getHeader('Content-Length') || 0
+		}\n`,
+		(err) => {
+			if (err) throw err;
+		}
+	);
 }
